42269f6529c040280078ef4f2f7748da0bcd213b
E6Iss <edismorina1@icloud.com>
2025-10-28 18:49:17 +0100

    Update: changed embed color to light royal blue

--- commands/ticketpanel.js
+++ commands/ticketpanel.js
@@ -22,7 +22,7 @@ module.exports = {
     }
     client.__panelCooldown.set(message.channel.id, Date.now());
 
-    const color = 0x06072C;
+   const color = 0x4A7DFF;
 
     const e1 = new EmbedBuilder()
       .setTitle('Welcome to MagicUI Support')
